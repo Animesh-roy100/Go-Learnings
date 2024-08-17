a991c75c223e5eae0a47813374f3df0e00250723
Animesh-roy100 <royanimesh7478@gmail.com>
2024-08-18 04:24:27 +0530

    small fix

--- books-store-ddd/internal/domain/book/repository.go
+++ books-store-ddd/internal/domain/book/repository.go
@@ -6,7 +6,7 @@ type Repository interface {
 	Save(book *Book) error
 	FindByID(id valueobjects.ID) (*Book, error)
 	FindByISBN(isbn valueobjects.ISBN) (*Book, error)
-	// List(limit, offset int) ([]*Book, error)
-	// Update(book *Book) error
-	// Delete(id valueobjects.ID) error
+	List(limit, offset int) ([]*Book, error)
+	Update(book *Book) error
+	Delete(id valueobjects.ID) error
 }
