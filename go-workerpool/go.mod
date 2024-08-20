e40b06645a4d65c46d4b70780f8f6892f887e186
Animesh-roy100 <royanimesh7478@gmail.com>
2024-08-21 03:31:05 +0530

    go workerpool added

---
+++ go-workerpool/go.mod
@@ -0,0 +1,3 @@
+module go-workerpool
+
+go 1.22.2
---
+++ go-workerpool/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+type Site struct {
+	URL string
+}
+
+type Result struct {
+	URL    string
+	Status int
+}
+
+func crawl(wId int, jobs <-chan Site, results chan<- Result) {
+	for site := range jobs {
+		log.Printf("Worker ID: %d\n", wId)
+		resp, err := http.Get(site.URL)
+		if err != nil {
+			log.Println(err.Error())
+		}
+		results <- Result{URL: site.URL, Status: resp.StatusCode}
+	}
+}
+
+func main() {
+	// fmt.Println("worker pools in go")
+
+	jobs := make(chan Site, 3)
+	results := make(chan Result, 3)
+
+	for w := 1; w <= 3; w++ {
+		go crawl(w, jobs, results)
+	}
+
+	urls := []string{
+		"https://google.com",
+		"https://stackoverflow.com",
+		"https://golang.org",
+		"https://example.com",
+	}
+
+	for _, url := range urls {
+		jobs <- Site{URL: url}
+	}
+	close(jobs)
+
+	for a := 1; a <= 4; a++ {
+		result := <-results
+		log.Println(result)
+	}
+}
